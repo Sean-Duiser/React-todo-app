5d3b46bd8e4025bb6d186e8b2bfba2efb6dc0da6
Sean Duiser <duzer84@gmail.com>
2022-06-21 11:40:40 -0400

    delete functionality

--- src/App.js
+++ src/App.js
@@ -4,46 +4,39 @@ import './App.css';
 import Todo from './Todo';
 import db from './firebase';
 import firebase from 'firebase';
-
 function App() {
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState('');
-
   useEffect(() => {
     db.collection('todos').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
-      setTodos(snapshot.docs.map(doc => doc.data().todo))
+      setTodos(snapshot.docs.map(doc => ({id: doc.id, todo: doc.data().todo})))
     })
-  }, []);
-
+      }, []);
   const addTodo = (event) => {
     event.preventDefault();
-
     db.collection('todos').add({
       todo: input,
       timestamp: firebase.firestore.FieldValue.serverTimestamp()
-})
-
+    })
     setInput('');
   }
   return (
     <div className="App">
-      <h1>Simple React Todo Organizer📘</h1>
+      <h1>Simple React Todo Organizer:blue_book:</h1>
       <form>
         <FormControl>
-          <InputLabel>✅ Write a Todo</InputLabel>
+          <InputLabel>:white_check_mark: Write a Todo</InputLabel>
           <Input value={input} onChange={event => setInput(event.target.value)} />
         </FormControl>
-
         <Button disabled={!input} type="submit" onClick={addTodo} variant="contained" color="primary">Add Todo
         </Button>
       </form>
       <ul>
         {todos.map(todo => (
-          <Todo text={todo} />
+          <Todo todo={todo} />
         ))}
       </ul>
     </div>
   );
 }
-
 export default App;
--- src/Todo.js
+++ src/Todo.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import { List, ListItem, ListItemAvatar, ListItemText} from '@mui/material';
+import { List, ListItem, ListItemAvatar, ListItemText, Button} from '@mui/material';
 import './Todo.css';
-
+import db from './firebase';
 function Todo(props) {
   return (
     <List className="todo_list">
       <ListItem>
         <ListItemAvatar>
         </ListItemAvatar>
-        <ListItemText primary={props.text} secondary="Dummy Deadline ⏰" />
+        <ListItemText primary={props.todo.todo} secondary="Dummy Deadline :alarm_clock:" />
       </ListItem>
+      <Button onClick={event => db.collection('todos').doc(props.todo.id).delete()}>DELETE POST</Button>
     </List>
   )
 }
-
 export default Todo
