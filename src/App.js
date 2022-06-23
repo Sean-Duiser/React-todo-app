9dd0f463c20e59fe6ff1628f7769b7ac47068d6e
Sean Duiser <duzer84@gmail.com>
2022-06-23 10:33:07 -0400

    full CRUD

--- src/App.js
+++ src/App.js
@@ -4,30 +4,39 @@ import './App.css';
 import Todo from './Todo';
 import db from './firebase';
 import firebase from 'firebase';
+
 function App() {
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState('');
+
   useEffect(() => {
     db.collection('todos').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
       setTodos(snapshot.docs.map(doc => ({id: doc.id, todo: doc.data().todo})))
     })
+    
       }, []);
+
+
+
   const addTodo = (event) => {
     event.preventDefault();
+
     db.collection('todos').add({
       todo: input,
       timestamp: firebase.firestore.FieldValue.serverTimestamp()
     })
+
     setInput('');
   }
   return (
     <div className="App">
-      <h1>Simple React Todo Organizer 📘</h1>
+      <h1>📘Simple React Todo Organizer</h1>
       <form>
         <FormControl>
-          <InputLabel>✔️ Write a Todo</InputLabel>
+          <InputLabel>✅ Write a Todo</InputLabel>
           <Input value={input} onChange={event => setInput(event.target.value)} />
         </FormControl>
+
         <Button disabled={!input} type="submit" onClick={addTodo} variant="contained" color="primary">Add Todo
         </Button>
       </form>
@@ -39,4 +48,5 @@ function App() {
     </div>
   );
 }
+
 export default App;
